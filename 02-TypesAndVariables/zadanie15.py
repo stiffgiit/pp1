e05d3ec75dabfd646e77d645a7a565aa6720863d
d k <kawuladominik50@gmail.com>
2023-10-28 23:54:26 +0200

    fourth commit

---
+++ 02-TypesAndVariables/zadanie15.py
@@ -0,0 +1,10 @@
+celcius = int(input("Enter a number: "))
+
+kelwin = celcius + 273,15
+
+farenheit = (celcius * 9/5) + 32
+temperature = (f"It's {celcius} degrees in Celcius, also it's {kelwin} deegres in kelvin, and it's {farenheit} degrees in farenheit")
+print(temperature)
+
+
+

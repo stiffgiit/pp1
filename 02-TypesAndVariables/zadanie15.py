07b65877b258664929ce9749edfaea8847a2415c
d k <kawuladominik50@gmail.com>
2023-10-29 18:43:23 +0100

    Fourth commit

--- 02-TypesAndVariables/zadanie15.py
+++ 02-TypesAndVariables/zadanie15.py
@@ -1,10 +1,12 @@
-celcius = int(input("Enter a number: "))
+celcius = int(input("Enter a number: "))        #ile chcemy wpisać stopni w celciuszu
 
-kelwin = celcius + 273,15
+kelwin = celcius + 273,15       #przeliczamy celciusza na kelvina  
 
-farenheit = (celcius * 9/5) + 32
-temperature = (f"It's {celcius} degrees in Celcius, also it's {kelwin} deegres in kelvin, and it's {farenheit} degrees in farenheit")
-print(temperature)
+farenheit = (celcius * 9/5) + 32  #przeliczamy celciusza na farenheita
+
+temperature = (f"It's {celcius} degrees in Celcius, also it's {kelwin} deegres in kelvin, and it's {farenheit} degrees in farenheit")   #przypisujemy zmienną "temperature" i używamy funkcji f string aby wrzucić przypisane powyższe wartości zmiennych
+print(temperature)      #otrzymujemy wynik z powyższej linijki
+
 
 
 

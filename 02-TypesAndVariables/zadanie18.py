c3afad2e004bbe851d1da1333e4d71c872f0d827
d k <kawuladominik50@gmail.com>
2023-10-29 18:56:33 +0100

    Fifth commit

--- 02-TypesAndVariables/zadanie18.py
+++ 02-TypesAndVariables/zadanie18.py
@@ -1,11 +1,11 @@
 x = 7
 y = 34
-print ("Value of x: ", x)
+print("Value of x: ", x)
 print("Value of y: ", y)
+
 
 temp = x
 x = y
 y = temp
-
 print("Value of x after swapping: ", format(x))
 print("Value of y after swapping: ", format(y))
---
+++ 02-TypesAndVariables/zadanie19.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the side number: "))
+volume = a**3
+area = 6*(a**2)
+print(f" The cube side is {a} \n Cube volumne: {volume} \n Cube surface area: {area} ")
+

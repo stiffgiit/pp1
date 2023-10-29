fe56b2db79584d96b8fdd07d58989e4905546ba9
d k <kawuladominik50@gmail.com>
2023-10-29 21:58:49 +0100

    Eleventh commit

--- 02-TypesAndVariables/zadanie24.py
+++ 02-TypesAndVariables/zadanie24.py
@@ -1,5 +1,5 @@
 tablica = input("Enter vehicle registration number: ")
-if tablica.startswith("KR"):
+if tablica.startswith("KR") or tablica.startswith("KK"):
     print("Car from Kraków: True")
 else:
     print("Car from Kraków: False")
---
+++ 02-TypesAndVariables/zadanie25.py
@@ -0,0 +1,5 @@
+age = int(input("Enter age: "))
+if age <= 26:
+    print("Exemption from paying taxes: True")
+else:
+    print("Exemption from paying taxes: False")

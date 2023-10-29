14a3a6b7819089d53581420fb25091cdddb27291
d k <kawuladominik50@gmail.com>
2023-10-29 22:08:56 +0100

    twelfth commit

---
+++ 02-TypesAndVariables/zadanie26.py
@@ -0,0 +1,5 @@
+number = int(input("Enter number: "))
+if (number%2) == 0:                    #sprawdzanie czy liczba jest parzysta
+    print("Number is even: True")
+else:
+    print("Number is even: False")

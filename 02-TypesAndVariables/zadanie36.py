8640cbad6d07b0e06a38860f2685890b3b834ab6
Dominik Kawula <146730219+stiffgiit@users.noreply.github.com>
2023-10-30 14:57:19 +0100

    Add files via upload

---
+++ 02-TypesAndVariables/zadanie36.py
@@ -0,0 +1,4 @@
+buy = float(input("Bank buys EUR: "))
+sold = float(input("Bank sells EUR: "))
+spread = sold-buy
+print(f"Spread: {spread: .4f}")   # .4f to 4 decimal places czyli miejsca po przecinku 

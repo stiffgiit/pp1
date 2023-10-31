6cc044370ad16629b97b0b4f8e193d4a77923087
d k <kawuladominik50@gmail.com>
2023-10-31 20:41:04 +0100

    Sixteenth commit

---
+++ 03-ControlStructures/zadanie8.py
@@ -0,0 +1,4 @@
+speed_limit = 140
+car_speed = int(input("Enter vehicle speed: "))
+if car_speed > speed_limit:
+    print("Warning: speed limit exceeded!")
---
+++ 03-ControlStructures/zadanie9.py
@@ -0,0 +1,6 @@
+x = 50
+number = int(input("Enter % of completed tasks: "))
+if number >= x:
+    print("Test passed")
+else:
+    print("Test failed")

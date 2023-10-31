33a75451258c2778ea68790eafb48800323c5a03
d k <kawuladominik50@gmail.com>
2023-10-31 21:53:31 +0100

    Eighteen commit

---
+++ 03-ControlStructures/zadanie13.py
@@ -0,0 +1,7 @@
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+if number1 >=0 or number2 >= 0:
+    print(f"At least one of the entered numbers {number1} and {number2} is not negative")
+else:
+    print("Both numbers are negative")
+      
---
+++ 03-ControlStructures/zadanie14.py
@@ -0,0 +1,4 @@
+i = 1
+while i <= 4:
+    print("Practice makes perfect!!")
+    i = i + 1

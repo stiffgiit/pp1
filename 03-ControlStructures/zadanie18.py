d2c0839f91a8c88df48b93211cfdf8e229d881c6
Dominik Kawula <146730219+stiffgiit@users.noreply.github.com>
2023-11-01 19:03:03 +0100

    Add files via upload

---
+++ 03-ControlStructures/zadanie18.py
@@ -0,0 +1,6 @@
+i = 1
+while i <= 10:
+    result = 1/i
+    print(f"1/{i} = {result}")
+    i += 1
+    
---
+++ 03-ControlStructures/zadanie19.py
@@ -0,0 +1,8 @@
+num = 0
+for i in range(1,10):
+    if i%2 == 0:
+        num += i
+        print(i)
+print("Sum of evens is: ",num)
+        
+    

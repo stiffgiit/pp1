f86fbb09fbd212e0a3194747159deaf2e1c60b5f
Dominik Kawula <146730219+stiffgiit@users.noreply.github.com>
2023-11-08 15:46:02 +0100

    Add files via upload

---
+++ 04-Subroutines/mykeyboard.py
@@ -0,0 +1,8 @@
+def read_number():
+    text = int(input("Enter a number: "))
+    text1 = int(input("Enter a number: "))
+    sum = text + text1
+    print(f"{text} + {text1} = {sum} ")
+
+if __name__ == "__main__":
+    read_number()
---
+++ 04-Subroutines/mymath.py
@@ -0,0 +1,10 @@
+import random
+def generate_number():
+    for i in range(1,10):
+        print(i)
+    
+        
+if __name__ == "__main__":
+    generate_number()
+    
+    
